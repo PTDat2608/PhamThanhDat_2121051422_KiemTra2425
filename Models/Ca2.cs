f56fa59d17aa9234dd358f9e15d52ab909160e89
PTDat2608 <ptdat26082k3@gmail.com>
2025-05-26 08:37:58 +0700

    Cau1_Ca2

---
+++ Models/Ca2.cs
@@ -0,0 +1,9 @@
+namespace PTD_Ktra.Models;
+
+public class Ca2
+{
+    [key]
+    public string MyProperty { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
